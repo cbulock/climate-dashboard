b87ffb0165ee15f8ee0cddcef765503142c6eb22
Cameron Bulock <cameron@bulock.com>
2022-07-16 22:00:46 -0400

    improvements for different screen sizes

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,21 +4,43 @@ import {
 	subscribeEntities,
 	createLongLivedTokenAuth,
 } from 'home-assistant-js-websocket';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+	faArrowUpFromWaterPump,
 	faDroplet,
 	faHotTubPerson,
 	faWaterLadder,
 } from '@fortawesome/free-solid-svg-icons';
 
+const pulse = keyframes`
+	0% {
+		opacity: 1;
+	}
+
+	70% {
+		opacity: 0.5;
+	}
+
+	100% {
+		opacity: 1;
+	}
+`;
+
 const HumidityIcon = styled(FontAwesomeIcon)`
-	padding-right: 0.3em;
+	padding-right: 1vw;
 `;
 
 const LargerIcon = styled(FontAwesomeIcon)`
-	padding-right: 0.2em;
+	padding-right: 0.8vw;
+`;
+
+const PumpIcon = styled(FontAwesomeIcon)`
+	font-size: 3vw;
+	justify-self: end;
+	padding-right: 3vw;
+	animation: ${pulse} 2s infinite;
 `;
 
 const TempContainer = styled.div`
@@ -34,7 +56,6 @@ const Temp = styled.div`
 `;
 
 const LocationLabel = styled.p`
-	font-size: 2rem;
 	font-variant-caps: small-caps;
 	margin: 0;
 `;
@@ -50,36 +71,43 @@ const Main = styled.main`
 	height: 100vh;
 	background: rgb(2, 204, 255);
 	color: white;
-	padding: 1em;
 `;
 
 const Outdoors = styled(TempContainer)`
 	grid-area: Outdoors;
 `;
 const OutdoorTemp = styled(Temp)`
-	font-size: 12rem;
+	font-size: 25vw;
 `;
 const OutdoorHumidity = styled.div`
-	font-size: 2rem;
+	font-size: 4vw;
 	justify-self: end;
+`;
+const OutdoorLabel = styled(LocationLabel)`
+	font-size: 4vw;
 `;
 
 const Indoors = styled(TempContainer)`
 	grid-area: Indoors;
 `;
 const IndoorTemp = styled(Temp)`
-	font-size: 8rem;
+	font-size: 18vw;
 `;
 const IndoorHumidity = styled.div`
-	font-size: 1.5rem;
+	font-size: 3.5vw;
 	justify-self: end;
+`;
+const IndoorLabel = styled(LocationLabel)`
+	font-size: 3.5vw;
 `;
 
 const Pool = styled.div`
+	width: fit-content;
 	grid-area: Pool;
+	display: grid;
 `;
 const PoolTemp = styled.div`
-	font-size: 5rem;
+	font-size: 12vw;
 	font-weight: 900;
 `;
 
@@ -87,7 +115,7 @@ const HotTub = styled.div`
 	grid-area: HotTub;
 `;
 const HotTubTemp = styled.div`
-	font-size: 5rem;
+	font-size: 12vw;
 	font-weight: 900;
 `;
 
@@ -115,7 +143,7 @@ const Dashboard = () => {
 				<OutdoorTemp>
 					{Math.round(entities['sensor.outdoor_temp']?.state)}°
 				</OutdoorTemp>
-				<LocationLabel>Outdoors</LocationLabel>
+				<OutdoorLabel>Outdoors</OutdoorLabel>
 				<OutdoorHumidity>
 					<HumidityIcon icon={faDroplet} />
 					{Math.round(entities['sensor.outdoor_humidity']?.state)}%
@@ -125,7 +153,7 @@ const Dashboard = () => {
 				<IndoorTemp>
 					{Math.round(entities['sensor.indoor_temp']?.state)}°
 				</IndoorTemp>
-				<LocationLabel>Indoors</LocationLabel>
+				<IndoorLabel>Indoors</IndoorLabel>
 				<IndoorHumidity>
 					<HumidityIcon icon={faDroplet} />
 					{Math.round(entities['sensor.indoor_humidity']?.state)}%
@@ -136,6 +164,9 @@ const Dashboard = () => {
 					<LargerIcon icon={faWaterLadder} />
 					{Math.round(entities['sensor.pool_temp']?.state)}°
 				</PoolTemp>
+				{entities['switch.pool_pump']?.state === 'on' && (
+					<PumpIcon icon={faArrowUpFromWaterPump} />
+				)}
 			</Pool>
 			<HotTub>
 				<HotTubTemp>
